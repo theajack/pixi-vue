33209f443397cf19c0be5cca791f2af96be751da
tackchen <tackchen@tencent.com>
2022-05-05 10:21:28 +0800

    feat: 删除旧代码

--- n-src/main.js
+++
@@ -1,8 +0,0 @@
-// import { createApp } from './three-renderer';
-// import App from "./App.vue";
-
-import {createApp} from './renderer';
-import App from './App1.vue';
-createApp(App).mount('#app');
-
-
