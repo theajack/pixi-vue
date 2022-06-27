3080a9ebf068d75eb9c73c21952d1a21c77a0cb6
tackchen <tackchen@tencent.com>
2022-06-28 07:39:52 +0800

    feat: package-lock

--- src/renderer/dom/img.ts
+++ src/renderer/dom/img.ts
@@ -1,15 +1,15 @@
 /*
  * @Author: tackchen
  * @Date: 2022-05-04 09:19:36
- * @LastEditors: tackchen
- * @LastEditTime: 2022-05-04 09:20:25
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2022-06-28 07:25:56
  * @FilePath: /pixi-vue/src/renderer/dom/img.ts
  * @Description: Coding something
  */
 
 
-import {PVElement} from "./base/element";
-
+import {PVElement} from './base/element';
+ 
 export class Img extends PVElement {
     
 }
